dd510dc7ee6b5e49b093f45d11a437f858275183
Jake Dibble <jdibble@bsu.edu>
2020-08-31 16:21:01 -0500

    Add vector impulse to player object

--- Player.gd
+++ Player.gd
@@ -1,5 +1,9 @@
-extends Area2D
+extends RigidBody2D
+export var strength : float = 500
 
-func _draw():
-	draw_circle(Vector2.ZERO, $CollisionShape2D.shape.radius, Color.aliceblue)
-	
+func _process(delta):
+	if Input.is_action_just_pressed("launch"):
+		var direction = Vector2(1,-1).normalized()
+		var velocity = direction * strength
+		apply_impulse(Vector2.ZERO,velocity)
+
