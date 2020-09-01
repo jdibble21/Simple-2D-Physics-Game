b5b18435ebe5240196b21aa3b06322bb39ca895a
Jake Dibble <jdibble@bsu.edu>
2020-09-01 17:34:14 -0400

    Add HUD scene

--- Player.gd
+++ Player.gd
@@ -3,16 +3,17 @@ export var strength : float = 500
 
 func _process(delta):
 	if Input.is_action_just_pressed("launch"):
+		var vector_one = get_node("HUD/AngleSlider").value
+		print(vector_one)
 		emit_signal("sleeping_state_changed")
-		var direction = Vector2(1,-1).normalized()
+		var direction = Vector2(vector_one,-1).normalized()		
 		var velocity = direction * strength
 		apply_impulse(Vector2.ZERO,velocity)
 		
 func _integrate_forces(state):
 	set_scale(Vector2(-1, 1))
 
-
-
 func _on_Player_sleeping_state_changed():
-	$AnimatedSprite.play("moving")
+	if Input.is_action_just_pressed("launch"):
+		$AnimatedSprite.play("moving")
 	
