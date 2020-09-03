7c36aa6b20d6a2c64756b6ec99ec4886006ec163
Jake Dibble <jdibble@bsu.edu>
2020-09-03 16:13:33 -0400

    Rework angle calculation

--- Player.gd
+++ Player.gd
@@ -12,6 +12,7 @@ func _process(delta):
 		var direction = Vector2(vector_one,-1).normalized()
 		var velocity = direction * strength
 		print("velocity: " + str(velocity))
+		calculateAngle(velocity.x,velocity.y)
 		apply_impulse(Vector2.ZERO,velocity)
 	if Input.is_action_just_pressed("reload_scene"):
 		get_tree().reload_current_scene()
@@ -23,3 +24,9 @@ func _on_Player_sleeping_state_changed():
 	if Input.is_action_just_pressed("launch"):
 		$AnimatedSprite.play("moving")
 
+func calculateAngle(x,y):
+	var div_val = y/x
+	print("pre calc div value: " + str(div_val))
+	var calculated_angle = atan(div_val)
+	print("calculated result: " + str(rad2deg(calculated_angle)))
+	
